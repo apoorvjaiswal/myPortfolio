e066f50c2059707e55e37b95371d4fb4c543a5fd
apoorv jaiswal <55631115+apoorvjaiswal@users.noreply.github.com>
2025-06-17 22:14:56 +0530

    Update ExperienceCard.js
    added experience of Finkeda

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -37,14 +37,15 @@ export default function ExperienceCard({cardInfo, isDark}) {
         <div className="experience-div-company">
           <h5 className="experience-text-company">{cardInfo.company}</h5>
         </div>
-
         <img
-          crossOrigin={"anonymous"}
+          crossOrigin="anonymous"
           ref={imgRef}
           className="experience-roundedimg"
+          style={cardInfo.company === "Finkeda" ? {objectFit: "fill"} : {}}
           src={cardInfo.companylogo}
           alt={cardInfo.company}
-          onLoad={() => getColorArrays()}
+          height="2rem"
+          onLoad={getColorArrays}
         />
       </div>
       <div className="experience-text-details">
